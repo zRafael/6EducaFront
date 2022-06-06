994e1d3cf0937baef4b4c136eff01f4391da8df6
ester-ferr <estereliege_ferr@hotmail.com>
2022-06-06 14:06:10 -0300

    Consumindo a segurança criada no back-end

--- 6Educa/src/app/app.module.ts
+++ 6Educa/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,8 @@ import { LoginComponent } from './login/login.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -17,10 +20,13 @@ import { HomeComponent } from './home/home.component';
     CadastrarComponent,
     RodapeComponent,
     HomeComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 6Educa/src/app/login/login.component.ts
+++ 6Educa/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+    console.log(this.userLogin.token)
+        console.log(this.userLogin.nome)
   }
 
+        
+
+  entrar() {
+    this.auth.entrar(this.userLogin).subscribe({
+      next:(resp: UserLogin)=>{
+        this.userLogin = resp
+
+        
+        environment.token = this.userLogin.token
+        environment.nome = this.userLogin.nome
+        environment.foto = this.userLogin.foto
+        environment.id = this.userLogin.id 
+        
+        
+        this.router.navigate(['/home'])
+        
+      },
+    
+      
+  error:erro => {
+    if(erro.status == 401){
+      alert('Usuário ou senha incorretos!')
+    }
+  },
+  });
+}
 }
